c525b719187dfbb3ae8b53e52d3048472afa3553
venkatamohan <venkatemohan89@gmail.com>
2025-12-24 19:17:40 +0530

    changes

--- restaurant/restaurant/settings.py
+++ restaurant/restaurant/settings.py
@@ -126,8 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",  # for login via admin
